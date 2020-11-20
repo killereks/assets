36b0a8e6fafafd5c23914a8b924e8bd82e1e4860
killereks <ssneeff@gmail.com>
2020-11-20 03:10:17 +0000

    anim

--- script.js
+++ script.js
@@ -43,6 +43,22 @@ function fadeScroll() {
     }
 
   });
+  
+  $('.fade-right').each(function (i) {
+
+    var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    /* If the object is completely visible in the window, fade it it */
+    if (bottom_of_window > bottom_of_object) {
+
+      $(this).animate({
+        'opacity': '1',
+        "right": "0px"
+      }, 500);
+    }
+
+  });
 }
 
 var navbarHeight = document.querySelector("#navbar").offsetTop;
